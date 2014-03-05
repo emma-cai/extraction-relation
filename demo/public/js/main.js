80058bc5f4ef1a48a44eee81d56a7832a86b1b4c
Michael Schmitz <michael@schmitztech.com>
2014-03-04 16:21:11 -0800

    Fix URL posting.

--- demo/public/js/main.js
+++ demo/public/js/main.js
@@ -1,13 +1,21 @@
 angular.module('extraction', ['ui.bootstrap']);
 
 var ExtractionCtrl = function($scope, $http) {
-  $scope.model = { }
+  $scope.model = { };
 
-  $scope.submit = function(path) {
-    $http.post("/" + path, $scope.model.text)
+  $scope.submitText = function() {
+    $http.post("/text", $scope.model.text)
       .success(function(data, status, headers, config) {
         $scope.response = data;
         $scope.responseString = angular.toJson(data, pretty=true);
-      })
-  }
-}
+      });
+  };
+
+  $scope.submitUrl = function() {
+    $http.post("/url", $scope.model.url)
+      .success(function(data, status, headers, config) {
+        $scope.response = data;
+        $scope.responseString = angular.toJson(data, pretty=true);
+      });
+  };
+};
