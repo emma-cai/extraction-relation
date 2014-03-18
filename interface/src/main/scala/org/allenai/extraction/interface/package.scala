a91d812223d2f0575bdbab3c5d239da518268167
Jesse Kinkead <jessek@vulcan.com>
2014-03-18 09:26:20 -0700

    Rename Noun -> NounPhrase, verb -> verbPhrase.

--- interface/src/main/scala/org/allenai/extraction/interface/package.scala
+++ interface/src/main/scala/org/allenai/extraction/interface/package.scala
@@ -12,21 +12,22 @@ package object interface {
 
   /** The subject or the object of a tuple.
     * @param string the tokenized text from the original source. May be empty in the case where this
-    *     is an inferred Noun.
+    *     is an inferred NounPhrase.
     * @param variableLabel a label for this subject or object. Present if it's referred to in
-    *     another related Noun, or if this is an inferred Noun.
+    *     another related NounPhrase, or if this is an inferred NounPhrase.
     * @param pronounResolution what this resolves to, if it's a pronoun
     */
-  case class Noun (string: Seq[Token], variableLabel: Option[String], pronounResolution: Seq[Token])
+  case class NounPhrase(string: Seq[Token], variableLabel: Option[String],
+    pronounResolution: Seq[Token])
 
   /** An extracted Tuple.
     * @param subject the subject of the tuple
-    * @param verb the relation for this tuple. Can be empty, e.g. for simple noun phrases.
+    * @param verbPhrase the relation for this tuple. Can be empty, e.g. for simple noun phrases.
     * @param directObject the object of the tuple, or None if this doesn't have an object
     * @param extraPhrases any extra context phrases pulled out of the sentence
     */
-  case class ExtractionTuple (subject: Noun, verb: Seq[Token], directObject: Option[Noun],
-    extraPhrases: Seq[Seq[Token]])
+  case class ExtractionTuple(subject: NounPhrase, verbPhrase: Seq[Token],
+    directObject: Option[NounPhrase], extraPhrases: Seq[Seq[Token]])
 
   /** A relation with some normalization attempted.
     * @param string the string from the original sentence. May be empty if the relation was inferred
@@ -34,7 +35,7 @@ package object interface {
     * @param normalizedRelation the normalized form of the relation. None if the relation couldn't
     *     be normalized.
     */
-  case class Relation (string: Seq[Token], normalizedRelation: Option[Symbol])
+  case class Relation(string: Seq[Token], normalizedRelation: Option[Symbol])
 
   /** A single extracted relation. This is essentially a recursive Tuple of depth 2. */
   case class ExtractionRule (antecedent: ExtractionTuple, relation: Relation,
