a0c5c6e13b7144cc04a29baffc59ad77a58b937e
Jesse Kinkead <jesse.kinkead@gmail.com>
2014-03-27 09:45:09 -0700

    Move converter, and add in coreference support.

--- ermine/src/main/scala/org/allenai/extraction/stanford/StanfordXmlToTtl.scala
+++ ermine/src/main/scala/org/allenai/extraction/stanford/StanfordXmlToTtl.scala
@@ -1,4 +1,4 @@
-package org.allenai.extraction.manager
+package org.allenai.extraction.stanford
 
 import scala.xml.Node
 import scala.xml.XML
@@ -19,7 +19,11 @@ object StanfordXmlToTtl {
       outputPrinter.println
     }
 
-    // TODO(jkinkead): Process coreferences.
+    // Coreferences are stored in a 'coreference' element; this is not a typo.
+    for (coreference <- (xml \\ "coreference" \ "coreference")) {
+      processCoreference(coreference, outputPrinter)
+      outputPrinter.println
+    }
   }
 
   /** A sentence in the stanford output consists of tokens, "basic" dependencies, and "collapsed"
@@ -60,48 +64,89 @@ object StanfordXmlToTtl {
     }
   }
 
+
+  /** Process a coreference element, with a format like:
+    *
+    * <coreference>
+    *   <mention representative="true">
+    *     <sentence>2</sentence>
+    *     <head>4</head>
+    *   </mention>
+    *   <mention>
+    *     <sentence>5</sentence>
+    *     <head>10</head>
+    *   </mention>
+    * </coreference>
+    *
+    * and prints output like:
+    *
+    * id:2.4 coref:ref id:2.4 .
+    * id:2.4 coref:ref id:5.10 .
+    */
+  def processCoreference(coreference: Node, output: PrintStream): Unit = {
+    // Find the representative mention's ID.
+    val rootIdOption: Option[String] = {
+      val representativeMention = (coreference \ "mention") find { mention =>
+        ((mention \ "@representative").headOption map { _.text }) == Some("true")
+      }
+
+      representativeMention match {
+        case Some(node) => getMentionId(node)
+        case None => None
+      }
+    }
+
+    rootIdOption map { rootId =>
+      for {
+        mention <- coreference \ "mention"
+        mentionId <- getMentionId(mention)
+      } {
+        output.println(s"${rootId} coref:ref ${mentionId} .")
+      }
+    }
+  }
+
   /** Processes a token element, with a format like:
     *
-    * <token id="3"> 
-    *   <word>grow</word> 
-    *   <lemma>grow</lemma> 
-    *   <CharacterOffsetBegin>13</CharacterOffsetBegin> 
-    *   <CharacterOffsetEnd>17</CharacterOffsetEnd> 
-    *   <POS>VB</POS> 
-    *   <NER>O</NER> 
-    * </token> 
+    * <token id="3">
+    *   <word>grow</word>
+    *   <lemma>grow</lemma>
+    *   <CharacterOffsetBegin>13</CharacterOffsetBegin>
+    *   <CharacterOffsetEnd>17</CharacterOffsetEnd>
+    *   <POS>VB</POS>
+    *   <NER>O</NER>
+    * </token>
     *
     * and returns output like:
     *
-    * :1.3 token:text "grow" .
-    * :1.3 token:lemma "grow" .
-    * :1.3 token:pos "VB" .
-    * :1.3 token:begin 13 .
-    * :1.3 token:end 17 . 
+    * id:1.3 token:text "grow" .
+    * id:1.3 token:lemma "grow" .
+    * id:1.3 token:pos "VB" .
+    * id:1.3 token:begin 13 .
+    * id:1.3 token:end 17 .
     *
     * with the first number being the provided sentence ID.
     */
   def processToken(sentenceId: String, token: Node): Seq[String] = {
     (for (idAttr <- token \ "@id") yield {
-      val tokenId = s":${sentenceId}.${idAttr.text}"
+      val tokenId = s"id:${sentenceId}.${idAttr.text}"
       // Start of each token declaration.
-      val tokenPrefix = s"${tokenId} token"
-      val words = childrenToTtl(token, "word", tokenPrefix, "text", quoteValue = true)
-      val lemmas = childrenToTtl(token, "lemma", tokenPrefix, "lemma", quoteValue = true)
-      val posTags = childrenToTtl(token, "POS", tokenPrefix, "pos", quoteValue = true)
-      val begins = childrenToTtl(token, "CharacterOffsetBegin", tokenPrefix, "begin",
+      val words = childrenToTtl(token, "word", tokenId, "token:text", quoteValue = true)
+      val lemmas = childrenToTtl(token, "lemma", tokenId, "token:lemma", quoteValue = true)
+      val posTags = childrenToTtl(token, "POS", tokenId, "token:pos", quoteValue = true)
+      val begins = childrenToTtl(token, "CharacterOffsetBegin", tokenId, "token:begin",
         quoteValue = false)
-      val ends = childrenToTtl(token, "CharacterOffsetEnd", tokenPrefix, "end", quoteValue = false)
+      val ends = childrenToTtl(token, "CharacterOffsetEnd", tokenId, "token:end",
+        quoteValue = false)
 
       // Ignore NER=O, otherwise print NE stuff.
       val nerText = ((token \ "NER").headOption map { _.text }).getOrElse("O")
       val nes: Seq[String] = nerText match {
         case "O" => Seq.empty
         case _: String => {
-          val nePrefix = s"${tokenId} ne"
           // Process type and normalization.
-          Seq(childrenToTtl(token, "NER", nePrefix, "type", quoteValue = true),
-            childrenToTtl(token, "NormalizedNER", nePrefix, "norm", quoteValue = true)).flatten
+          Seq(childrenToTtl(token, "NER", tokenId, "ne:type", quoteValue = true),
+            childrenToTtl(token, "NormalizedNER", tokenId, "ne:norm", quoteValue = true)).flatten
         }
         case _ => Seq.empty
       }
@@ -112,14 +157,14 @@ object StanfordXmlToTtl {
 
   /** Processes a dependency element, with a format like:
     *
-    * <dep type="det"> 
+    * <dep type="det">
     *   <governor idx="2">animals</governor>
     *   <dependent idx="1">Some</dependent>
     * </dep>
     *
     * and returns output like:
     *
-    * :1.2 basic:det :1.1 .
+    * id:1.2 basic:det id:1.1 .
     *
     * with the first number in both labels being the provided sentence ID, and the first part of
     * the middle label being the given dependencyLabel.
@@ -132,14 +177,15 @@ object StanfordXmlToTtl {
       val governorId = (dependency \ "governor" \ "@idx").head
       val dependentId = (dependency \ "dependent" \ "@idx").head
 
-      s":${sentenceId}.${governorId} ${dependencyLabel}:${typeAttr} :${sentenceId}.${dependentId} ."
+      s"id:${sentenceId}.${governorId} ${dependencyLabel}:${typeAttr} " +
+        s"id:${sentenceId}.${dependentId} ."
     }
   }
 
   /** Prints declaration headers. */
   def printHeaders(outputPrinter: PrintStream): Unit = {
     outputPrinter.print("""
-@prefix : <http://halo.vulcan.com/id#> .
+@prefix id: <http://aristo.allenai.org/id#> .
 
 @prefix token: <http://nlp.stanford.edu/token/> .
 @prefix ne: <http://nlp.stanford.edu/ne/> .
@@ -154,19 +200,31 @@ object StanfordXmlToTtl {
     *
     * @param node the parent node to look in
     * @param childName the name of the child node to look for
-    * @param prefix the declaration prefix
+    * @param tokenId the ID of the token
     * @param outputName the output name
     * @param quoteValue if true, quote the child node's value
     */
-  def childrenToTtl(node: Node, childName: String, prefix: String, outputName: String,
+  def childrenToTtl(node: Node, childName: String, tokenId: String, outputName: String,
     quoteValue: Boolean): Seq[String] = {
     for {
       child <- node \ childName
       value = child.text
     } yield if (quoteValue) {
-      s"""${prefix}:${outputName} "${value}" ."""
+      s"""${tokenId} ${outputName} "${value}" ."""
     } else {
-      s"${prefix}:${outputName} ${value} ."
+      s"${tokenId} ${outputName} ${value} ."
     }
+  }
+
+  /** Returns the ID for a given mention element. This pulls the values of the 'sentence' and 'head'
+    * child nodes for the ID.
+    */
+  def getMentionId(mention: Node): Option[String] = {
+    val sentenceIdOption = (mention \ "sentence").headOption map { _.text }
+    val headIdOption = (mention \ "head").headOption map { _.text }
+    for {
+      sentenceId <- sentenceIdOption
+      headId <- headIdOption
+    } yield s"id:${sentenceId}.${headId}"
   }
 }
