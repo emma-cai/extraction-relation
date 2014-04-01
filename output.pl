e2c1c958da178f8749f130855b51cfcd66cb6c1b
Kevin Humphreys <kevinh@Kevins-MacBook-Pro.local>
2014-03-31 17:42:59 -0700

    add dummy confidence values

--- output.pl
+++ output.pl
@@ -195,12 +195,12 @@ text_tuple([S,Verb,Arg|Mods],Text) :-
 	!.
 
 
-json_tuple(Ent,json([class='ExtractionTuple',subject=Json])) :-
+json_tuple(Ent,json([class='ExtractionTuple',confidence='1.0',subject=Json])) :-
 	atom(Ent), !,
 	json_arg(Ent,Json).
 json_tuple([S,V],Json) :-
 	json_tuple([S,V,[]],Json).
-json_tuple([S,Verb,Arg|Mods],json([class='ExtractionTuple',subject=SubjJson,verbPhrase=VerbTokenIds|RestJson])) :-
+json_tuple([S,Verb,Arg|Mods],json([class='ExtractionTuple',confidence='1.0',subject=SubjJson,verbPhrase=VerbTokenIds|RestJson])) :-
 	json_arg(S,SubjJson),
 	json_verb(Verb,VerbTokenIds,V),
 	( (rdf(_,basic:cop,V),
@@ -247,7 +247,7 @@ text_entity(Arg,Text) :-
 	entity_tokens(Arg,Tokens),
 	tokens_text_quoted(Tokens,Text).
 
-json_entity(Arg,json([class='ExtractionTuple',subject=json([class='NounPhrase',string=TokenIds]),verbPhrase=[]])) :-
+json_entity(Arg,json([class='ExtractionTuple',confidence='1.0',subject=json([class='NounPhrase',string=TokenIds]),verbPhrase=[]])) :-
 	entity_tokens(Arg,Tokens),
 	prefixed_ids(Tokens,TokenIds).
 
