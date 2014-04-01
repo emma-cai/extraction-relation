f20088993e777f91f18ed984f57978c7e9c77493
Kevin Humphreys <kevinh@Kevins-MacBook-Pro.local>
2014-04-01 14:25:58 -0700

    create json Coreference

--- output.pl
+++ output.pl
@@ -226,10 +226,14 @@ text_arg(Arg,Text) :-
 	tokens_text_quoted(Tokens,Text).
 
 json_arg([],json([])) :- !.
-json_arg(_Arg-Var-true,json([class='NounPhrase',string=[],isInferred=(@true),coreferences=[Var]])) :- !.
-json_arg(Arg-Var,json([class=Class,string=TokenIds,isInferred=(@false),coreferences=[Var]])) :- !,
+json_arg(Arg-Var-true,json([class=Class,string=[],isInferred=(@true),
+			    coreferences=[json([class='Coreference',label=Var,sourceTokens=TokenIds])]])) :- !,
 	json_arg(Arg,json([class=Class,string=TokenIds|_])).
-json_arg(Arg,json([class='NounPhrase',string=TokenIds,isInferred=(@false),coreferences=[]])) :-
+json_arg(Arg-Var,json([class=Class,string=TokenIds,isInferred=(@false),
+		       coreferences=[json([class='Coreference',label=Var])]])) :- !,
+	json_arg(Arg,json([class=Class,string=TokenIds|_])).
+json_arg(Arg,json([class='NounPhrase',string=TokenIds,isInferred=(@false),
+		   coreferences=[]])) :-
 	arg_tokens(Arg,Tokens),
 	prefixed_ids(Tokens,TokenIds).
 
