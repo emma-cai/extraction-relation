b4126841aea4ed714de1276a4acbb3ccf70c4d5b
Kevin Humphreys <kevinh@Kevins-MacBook-Pro.local>
2014-04-18 15:23:30 -0700

    separate output types

--- regressions.sh
+++ regressions.sh
@@ -1,13 +1,23 @@
-swipl -q -l relation.pl -g "consult('patterns-stanford.pl'),rdf_load('regressions/ie-target.txt.rnn.ttl'),findall(_,relation,_),halt" > regressions/ie-target.txt.rnn.inf.new
-diff -qs regressions/ie-target.txt.rnn.inf regressions/ie-target.txt.rnn.inf.new
+swipl -q -l relation.pl -g "consult('patterns-stanford.pl'),rdf_load('sample/sample.ttl'),findall(_,(relation(J),write(J),nl,nl),_),halt" > sample/sample.out
 
-swipl -q -l relation.pl -g "consult('patterns-stanford.pl'),rdf_load('regressions/ie-target.txt.rnn.ttl'),findall(_,(relation(J),write(J),nl,nl),_),halt" > regressions/ie-target.txt.rnn.inf.new
-diff -qs regressions/ie-target.txt.rnn.json regressions/ie-target.txt.rnn.json.new
+for f in sample/*.out
+  do grep -E '^[;%]' $f > $f.txt
+     grep -E '^(english\(|rule[0-9]+:: )' $f > $f.inf
+     grep '^{"class":' $f > $f.json
+     head $f | grep '^@' > $f.ttl
+     grep -E '^[<	]' $f >> $f.ttl
+done
 
+swipl -q -l relation.pl -g "consult('patterns-stanford.pl'),rdf_load('regressions/ie-target.txt.rnn.ttl'),findall(_,(relation(J),write(J),nl,nl),_),halt" > regressions/ie-target.txt.rnn.out
 
-swipl -q -l relation.pl -g "consult('patterns-stanford.pl'),rdf_load('regressions/barrons.txt.rnn.ttl'),findall(_,relation,_),halt" > regressions/barrons.txt.rnn.inf.new
-diff -qs regressions/barrons.txt.rnn.inf regressions/barrons.txt.rnn.inf.new
+swipl -q -l relation.pl -g "consult('patterns-stanford.pl'),rdf_load('regressions/june2014.txt.ttl'),findall(_,(relation(J),write(J),nl,nl),_),halt" > regressions/june2014.txt.out
 
-swipl -q -l relation.pl -g "consult('patterns-sapir.pl'),rdf_load('regressions/barrons.txt.sapir.ttl'),findall(_,relation,_),halt" > regressions/barrons.txt.sapir.new
-diff -qs regressions/barrons.txt.sapir.out regressions/barrons.txt.sapir.new
+swipl -q -l relation.pl -g "consult('patterns-stanford.pl'),rdf_load('regressions/barrons.txt.rnn.ttl'),findall(_,(relation(J),write(J),nl,nl),_),halt" > regressions/barrons.txt.rnn.out
 
+for f in regressions/*.out
+  do grep -E '^[;%]' $f > $f.txt
+     grep -E '^(english\(|rule[0-9]+:: )' $f > $f.inf
+     grep '^{"class":' $f > $f.json
+     head $f | grep '^@' > $f.ttl
+     grep -E '^[<	]' $f >> $f.ttl
+done
--- sample/README.md
+++ sample/README.md
@@ -2,6 +2,9 @@
 input text: sample.txt
 stanford output: sample.xml
 xslt output: sample.ttl (also sample.dep for a human readable version)
-prolog text output: sample.out
-prolog json output: sample.json
+prolog text output: sample.out.txt
+prolog inference output: sample.out.inf
+prolog json output: sample.out.json
+prolog turtle output: sample.out.ttl
+all prolog output: sample.out
 ```
