83f7afa9ceb19ada666ef26d38d21a926f61e54f
Kevin Humphreys <kevinh@Kevins-MacBook-Pro.local>
2014-04-22 15:22:01 -0700

    add source ids to inference rules

--- regressions.sh
+++ regressions.sh
@@ -2,11 +2,12 @@ swipl -q -l relation.pl -g "consult('patterns-stanford.pl'),rdf_load('sample/sam
 
 for f in sample/*.out
   do grep -E '^[;%]' $f > $f.txt
-     grep -E '^(english\(|rule[0-9]+:: )' $f > $f.inf
+     grep -E '^(english\(|rule[0-9]+:: )' $f | gsed -E "s/^(english[(])?rule/\1sample.rule/" > $f.inf
      grep '^{"class":' $f > $f.json
      head $f | grep '^@' > $f.ttl
      grep -E '^[<	]' $f >> $f.ttl
 done
+
 
 swipl -q -l relation.pl -g "consult('patterns-stanford.pl'),rdf_load('regressions/ie-target.txt.rnn.ttl'),findall(_,(relation(J),write(J),nl,nl),_),halt" > regressions/ie-target.txt.rnn.out
 
@@ -14,11 +15,9 @@ swipl -q -l relation.pl -g "consult('patterns-stanford.pl'),rdf_load('regression
 
 swipl -q -l relation.pl -g "consult('patterns-stanford.pl'),rdf_load('regressions/barrons.txt.rnn.ttl'),findall(_,(relation(J),write(J),nl,nl),_),halt" > regressions/barrons.txt.rnn.out
 
-swipl -q -l relation.pl -g "consult('patterns-stanford.pl'),rdf_load('regressions/decomposed-sentences-all.txt.ttl'),findall(_,(relation(J),write(J),nl,nl),_),halt" > regressions/decomposed-sentences-all.txt.out
-
 for f in regressions/*.out
   do grep -E '^[;%]' $f > $f.txt
-     grep -E '^(english\(|rule[0-9]+:: )' $f > $f.inf
+     grep -E '^(english\(|rule[0-9]+:: )' $f | gsed -E "s/^(english[(])?rule/\1`basename $f`.rule/;s/\.txt(\.rnn)?\.out//" > $f.inf
      grep '^{"class":' $f > $f.json
      head $f | grep '^@' > $f.ttl
      grep -E '^[<	]' $f >> $f.ttl
