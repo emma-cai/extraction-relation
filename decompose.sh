9639a927619eb6213241aecf6580808f2e1096f4
Kevin Humphreys <kevinh@Kevins-MacBook-Pro.local>
2014-04-25 15:39:30 -0700

    convert decomposer output to prolog goal

--- decompose.sh
+++ decompose.sh
@@ -1,1 +1,4 @@
-while read line; do curl http://www.halotestframework.net:8003/decompose?question=$line; done | sed 's/Is it true that //;s/?"/."/'
+while read line; do curl http://www.halotestframework.net:8003/decompose?question=$line; done | \
+sed 's/Is it true that //;s/?"/."/' | \
+jq '.[] | "extract(\"\(.question)\", \"\(.focus)\")."' | \
+sed -E "s/'/\\\'/g;s/^\"//;s/\"$//;s/[\\]\"/'/g"
