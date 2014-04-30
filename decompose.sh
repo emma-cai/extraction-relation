2c2e4f0af52a73a33bd12be434425fa50d9a27e5
Kevin Humphreys <kevinh@Kevins-MacBook-Pro.local>
2014-04-30 15:25:09 -0700

    unused script

--- decompose.sh
+++
@@ -1,4 +0,0 @@
-while read line; do curl http://www.halotestframework.net:8003/decompose?question=$line; done | \
-sed 's/Is it true that //;s/?"/."/' | \
-jq '.[] | "extract(\"\(.question)\", \"\(.focus)\")."' | \
-sed -E "s/'/\\\'/g;s/^\"//;s/\"$//;s/[\\]\"/'/g"
