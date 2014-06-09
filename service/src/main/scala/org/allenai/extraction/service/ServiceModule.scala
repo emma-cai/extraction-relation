7c23bad1ebd582a05a8e5d60a94d8007963cb7ca
Jesse Kinkead <jessek@vulcan.com>
2014-06-09 16:58:56 -0700

    Add /info routing to Ermine service.

--- service/src/main/scala/org/allenai/extraction/service/ServiceModule.scala
+++ service/src/main/scala/org/allenai/extraction/service/ServiceModule.scala
@@ -1,8 +1,11 @@
 package org.allenai.extraction.service
 
 import org.allenai.common.Config._
+import org.allenai.common.{ InfoRoute, Version }
 import org.allenai.extraction.ConfigModule
 
+import akka.actor.ActorSystem
+import akka.event.Logging
 import com.escalatesoft.subcut.inject.{ BindingId, NewBindingModule }
 import com.typesafe.config.Config
 
@@ -15,16 +18,26 @@ object ServiceModuleId {
 }
 
 /** Module injecting the configuration for the Ermine service. */
-object ServiceModule extends NewBindingModule(module => {
+class ServiceModule(actorSystem: ActorSystem) extends NewBindingModule(module => {
   import module._
 
+  val log = Logging(actorSystem, classOf[ServiceModule])
+
   // Load our binding for Config.
-  module <~ ConfigModule
-  val config = inject[Config](None)
+  val config = ConfigModule.inject[Config](None)
 
   // Port the service will run on.
   bind[Int] idBy ServiceModuleId.Port toSingle config[Int]("ermine.port")
 
   // Pipeline configurations.
   bind[Config] idBy ServiceModuleId.Pipelines toSingle config[Config]("ermine.pipelines")
+
+  // Load our version & create the info route for it.
+  try {
+    val version = Version.fromResources("org.allenai.extraction", "extraction-manager-service")
+    val infoRoute = new InfoRoute().withVersion(version)
+    bind[InfoRoute] toSingle infoRoute
+  } catch {
+    case e: IllegalArgumentException => log.error(e, "failure loading infoRoute:")
+  }
 })
