2b1b3637144d6822783ecc56ca1cfe2e3302ffc9
sbhaktha <sumithrab@allenai.org>
2014-06-20 16:40:56 -0700

    Changes based on code review

--- ermine/src/main/scala/org/allenai/extraction/processors/definition/OtterExtractionsPostprocessingUtility.scala
+++ ermine/src/main/scala/org/allenai/extraction/processors/definition/OtterExtractionsPostprocessingUtility.scala
@@ -1,6 +1,13 @@
 package org.allenai.extraction.processors.definition
 
 object OtterExtractionsPostprocessingUtility {
+
+  /** List of words that if present as the object of an IsA relation by themselves,
+    * with no other information present, do not make useful extractions. Such
+    * extractions should be removed from the results.
+    */
+  val relObjBlacklist = Set("part", "piece", "thing", "something")
+
   /** This is a semantic postprocessing method to
     * remove low-quality extraction tuples from a given
     * sequence of extractions output by the definition
@@ -10,15 +17,10 @@ object OtterExtractionsPostprocessingUtility {
     * The method returns a clean set of extraction tuples.
     */
   def postProcess(rawExtractions: Seq[OtterExtractionTuple]): Seq[OtterExtractionTuple] = {
-    var postProcessedExtractions = Seq.empty[OtterExtractionTuple]
-
-    for (rawExtraction <- rawExtractions) {
-      val postProcessedExtractionOption = processExtraction(rawExtraction)
-      if (postProcessedExtractionOption.isDefined) {
-        postProcessedExtractions :+= postProcessedExtractionOption.get
-      }
-    }
-    postProcessedExtractions
+    for {
+      rawExtraction <- rawExtractions
+      postProcessedExtraction <- processExtraction(rawExtraction)
+    } yield postProcessedExtraction
   }
 
   /** Identifies tuples that are completely invalid or need to be modified somewhat.
@@ -30,10 +32,7 @@ object OtterExtractionsPostprocessingUtility {
     extraction match {
       // Invalid tuples: (x, isa, part), (x, isa, something), (x, isa, thing)
       case SimpleOtterExtractionTuple(_, _, _, relation, Some(relObj), advps, pps) if (relation.relationType.isDefined && (relation.relationType.get == RelationTypeEnum.IsA)
-        && (advps.size == 0) && (pps.size == 0) && (relObj.string.equalsIgnoreCase("part")
-          || relObj.string.equalsIgnoreCase("piece")
-          || relObj.string.equalsIgnoreCase("thing")
-          || relObj.string.equalsIgnoreCase("something"))) =>
+        && (advps.size == 0) && (pps.size == 0) && (relObjBlacklist.contains(relObj.string.toLowerCase))) =>
         None
       // Tuples to be modified: (x, isa, somebody), etc: to be modified to (x, isa, person)
       case SimpleOtterExtractionTuple(tokens, interval, agent, relation, Some(relObj), advps, pps) if (relation.relationType.isDefined && (relation.relationType.get == RelationTypeEnum.IsA)
