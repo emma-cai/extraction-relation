bf5c215c0b401e9240599d9a783a4b674eb0b8b1
Kevin Humphreys <kevinh@Kevins-MacBook-Pro.local>
2014-06-24 14:46:33 -0700

    question parses

---
+++ ermine/src/main/prolog/regressions/june2014questions.sh
@@ -0,0 +1,100 @@
+# QUESTION: Which force causes rocks to roll downhill? (A) gravity(B) friction(C) erosion(D) magnetism
+swipl -q -l extract.pl -g "extract('gravity causes rocks to roll downhill.', 'gravity')"
+swipl -q -l extract.pl -g "extract('friction causes rocks to roll downhill.', 'friction')"
+swipl -q -l extract.pl -g "extract('erosion causes rocks to roll downhill.', 'erosion')"
+swipl -q -l extract.pl -g "extract('magnetism causes rocks to roll downhill.', 'magnetism')"
+# QUESTION: Which activity is an example of a good health habit? (A) watching television(B) smoking cigarettes(C) eating candy(D) exercising every day
+swipl -q -l extract.pl -g "extract('watching television is an example of a good health habit.', 'watching television')"
+swipl -q -l extract.pl -g "extract('smoking cigarettes is an example of a good health habit.', 'smoking cigarettes')"
+swipl -q -l extract.pl -g "extract('eating candy is an example of a good health habit.', 'eating candy')"
+swipl -q -l extract.pl -g "extract('exercising every day is an example of a good health habit.', 'exercising every day')"
+# QUESTION: A decomposer is an organism that (A) hunts and eats animals(B) migrates for the winter(C) breaks down dead plants and animals(D) uses water and sunlight to make food
+swipl -q -l extract.pl -g "extract('a decomposer is an organism that hunts and eats animals.', 'hunts and eats animals')"
+swipl -q -l extract.pl -g "extract('a decomposer is an organism that migrates for the winter.', 'migrates for the winter')"
+swipl -q -l extract.pl -g "extract('a decomposer is an organism that breaks down dead plants and animals.', 'breaks down dead plants and animals')"
+swipl -q -l extract.pl -g "extract('a decomposer is an organism that uses water and sunlight to make food.', 'uses water and sunlight to make food')"
+# QUESTION: What is one way to change water from a liquid to a solid? (A) decrease the temperature (B) increase the temperature (C) decrease the mass (D) increase the mass
+swipl -q -l extract.pl -g "extract('decreasing the temperature is one way to change water from a liquid to a solid.', 'decreasing the temperature')"
+swipl -q -l extract.pl -g "extract('increasing the temperature is one way to change water from a liquid to a solid.', 'increasing the temperature')"
+swipl -q -l extract.pl -g "extract('decreasing the mass is one way to change water from a liquid to a solid.', 'decreasing the mass')"
+swipl -q -l extract.pl -g "extract('increasing the mass is one way to change water from a liquid to a solid.', 'increasing the mass')"
+# QUESTION: A student drops a ball. Which force causes the ball to fall to the ground? (A) electricity (B) friction (C) gravity (D) magnetism
+swipl -q -l extract.pl -g "extract('A student drops a ball. electricity causes the ball to fall to the ground.', 'electricity')"
+swipl -q -l extract.pl -g "extract('A student drops a ball. friction causes the ball to fall to the ground.', 'friction')"
+swipl -q -l extract.pl -g "extract('A student drops a ball. gravity causes the ball to fall to the ground.', 'gravity')"
+swipl -q -l extract.pl -g "extract('A student drops a ball. magnetism causes the ball to fall to the ground.', 'magnetism')"
+# QUESTION: What is the main source of energy for the water cycle? (A) electricity(B) erosion(C) gravity(D) sunlight
+swipl -q -l extract.pl -g "extract('electricity is the main source of energy for the water cycle.', 'electricity')"
+swipl -q -l extract.pl -g "extract('erosion is the main source of energy for the water cycle.', 'erosion')"
+swipl -q -l extract.pl -g "extract('gravity is the main source of energy for the water cycle.', 'gravity')"
+swipl -q -l extract.pl -g "extract('sunlight is the main source of energy for the water cycle.', 'sunlight')"
+# QUESTION: Which instrument should a student use to measure exactly 10 milliliters (mL) of water? (A) graduated cylinder (B) pan balance (C) ruler (D) thermometer
+swipl -q -l extract.pl -g "extract('a student should use a graduated cylinder to measure exactly 10 milliliters (mL) of water.', 'graduated cylinder')"
+swipl -q -l extract.pl -g "extract('a student should use pan balance to measure exactly 10 milliliters (mL) of water.', 'pan balance')"
+swipl -q -l extract.pl -g "extract('a student should use a ruler to measure exactly 10 milliliters (mL) of water.', 'ruler')"
+swipl -q -l extract.pl -g "extract('a student should use a thermometer to measure exactly 10 milliliters (mL) of water.', 'thermometer')"
+# QUESTION: Which activity is necessary for humans to maintain good health? (A) reading (B) resting (C) driving (D) talking
+swipl -q -l extract.pl -g "extract('reading is necessary for humans to maintain good health.', 'reading')"
+swipl -q -l extract.pl -g "extract('resting is necessary for humans to maintain good health.', 'resting')"
+swipl -q -l extract.pl -g "extract('driving activity is necessary for humans to maintain good health.', 'driving')"
+swipl -q -l extract.pl -g "extract('talking activity is necessary for humans to maintain good health.', 'talking')"
+# QUESTION: Green plants get the energy they need to make food from (A) air (B) sunlight (C) water (D) soil
+swipl -q -l extract.pl -g "extract('green plants get the energy they need to make food from air.', 'air')"
+swipl -q -l extract.pl -g "extract('green plants get the energy they need to make food from sunlight.', 'sunlight')"
+swipl -q -l extract.pl -g "extract('green plants get the energy they need to make food from water.', 'water')"
+swipl -q -l extract.pl -g "extract('green plants get the energy they need to make food from soil.', 'soil')"
+# QUESTION: When plants and animals die, which organisms help return nutrients to the food chain? (A) decomposers (B) predators (C) prey (D) producers
+swipl -q -l extract.pl -g "extract('when plants and animals die, decomposers help return nutrients to the food chain.', 'decomposers')"
+swipl -q -l extract.pl -g "extract('when plants and animals die, predators help return nutrients to the food chain.', 'predators')"
+swipl -q -l extract.pl -g "extract('when plants and animals die, prey help return nutrients to the food chain.', 'prey')"
+swipl -q -l extract.pl -g "extract('when plants and animals die, producers help return nutrients to the food chain.', 'producers')"
+# QUESTION: Which animal is preparing for a seasonal change in the environment? (A) a bat flying at night (B) a deer drinking water (C) an owl eating a mouse (D) a squirrel storing nuts
+swipl -q -l extract.pl -g "extract('a bat flying at night is preparing for a seasonal change in the environment.', 'a bat flying at night')"
+swipl -q -l extract.pl -g "extract('a deer drinking water is preparing for a seasonal change in the environment.', 'a deer drinking water')"
+swipl -q -l extract.pl -g "extract('an owl eating a mouse is preparing for a seasonal change in the environment.', 'an owl eating a mouse')"
+swipl -q -l extract.pl -g "extract('a squirrel storing nuts is preparing for a seasonal change in the environment.', 'a squirrel storing nuts')"
+# QUESTION: What form of energy is being used when a person pushes a wooden block across the floor? (A) mechanical (B) magnetic (C) sound (D) electrical
+swipl -q -l extract.pl -g "extract('mechanical energy is being used when a person pushes a wooden block across the floor.', 'mechanical')"
+swipl -q -l extract.pl -g "extract('magnetic energy is being used when a person pushes a wooden block across the floor.', 'magnetic')"
+swipl -q -l extract.pl -g "extract('sound is being used when a person pushes a wooden block across the floor.', 'sound')"
+swipl -q -l extract.pl -g "extract('electrical energy is being used when a person pushes a wooden block across the floor.', 'electrical')"
+# QUESTION: Since green plants make their own food, they are called (A) predators (B) prey (C) decomposers (D) producers
+swipl -q -l extract.pl -g "extract('since green plants make their own food, they are called predators.', 'predators')"
+swipl -q -l extract.pl -g "extract('since green plants make their own food, they are called prey.', 'prey')"
+swipl -q -l extract.pl -g "extract('since green plants make their own food, they are called decomposers.', 'decomposers')"
+swipl -q -l extract.pl -g "extract('since green plants make their own food, they are called producers.', 'producers')"
+# QUESTION: Which human activity most often has a harmful effect on the environment? (A) breathing (B) growing (C) planting (D) polluting
+swipl -q -l extract.pl -g "extract('breathing most often has a harmful effect on the environment.', 'breathing')"
+swipl -q -l extract.pl -g "extract('growing most often has a harmful effect on the environment.', 'growing')"
+swipl -q -l extract.pl -g "extract('planting most often has a harmful effect on the environment.', 'planting')"
+swipl -q -l extract.pl -g "extract('polluting most often has a harmful effect on the environment.', 'polluting')"
+# QUESTION: A dog opens its mouth and lets its tongue hang out. A human's body produces sweat. These are two ways that organisms may adjust to (A) cold temperatures (B) hot temperatures (C) a shortage of food (D) a shortage of drinking water
+swipl -q -l extract.pl -g "extract('A dog opens its mouth and lets its tongue hang out. A human\'s body produces sweat. these are two ways that organisms may adjust to cold temperatures.', 'cold temperatures')"
+swipl -q -l extract.pl -g "extract('A dog opens its mouth and lets its tongue hang out. A human\'s body produces sweat. these are two ways that organisms may adjust to hot temperatures.', 'hot temperatures')"
+swipl -q -l extract.pl -g "extract('A dog opens its mouth and lets its tongue hang out. A human\'s body produces sweat. these are two ways that organisms may adjust to a shortage of food.', 'a shortage of food')"
+swipl -q -l extract.pl -g "extract('A dog opens its mouth and lets its tongue hang out. A human\'s body produces sweat. these are two ways that organisms may adjust to a shortage of drinking water.', 'a shortage of drinking water')"
+# QUESTION: A student is growing some plants for an experiment. She notices small white spots on the leaves. Which tool should she use to get a better look at the spots? (A) thermometer (B) hand lens (C) graduated cylinder (D) balance
+swipl -q -l extract.pl -g "extract('A student is growing some plants for an experiment. She notices small white spots on the leaves. she should use a thermometer to get a better look at the spots.', 'thermometer')"
+swipl -q -l extract.pl -g "extract('A student is growing some plants for an experiment. She notices small white spots on the leaves. she should use a hand lens to get a better look at the spots.', 'hand lens')"
+swipl -q -l extract.pl -g "extract('A student is growing some plants for an experiment. She notices small white spots on the leaves. she should use a graduated cylinder to get a better look at the spots.', 'graduated cylinder')"
+swipl -q -l extract.pl -g "extract('A student is growing some plants for an experiment. She notices small white spots on the leaves. she should use a balance to get a better look at the spots.', 'balance')"
+# QUESTION: A fox grows thicker fur as a season changes. This adaptation helps the fox to (A) find food(B) keep warmer(C) grow stronger(D) escape from predators
+swipl -q -l extract.pl -g "extract('A fox grows thicker fur as a season changes. this adaptation helps the fox to find food.', 'find food')"
+swipl -q -l extract.pl -g "extract('A fox grows thicker fur as a season changes. this adaptation helps the fox to keep warmer.', 'keep warmer')"
+swipl -q -l extract.pl -g "extract('A fox grows thicker fur as a season changes. this adaptation helps the fox to grow stronger.', 'grow stronger')"
+swipl -q -l extract.pl -g "extract('A fox grows thicker fur as a season changes. this adaptation helps the fox to escape from predators.', 'escape from predators')"
+# QUESTION: Which adaptation often helps an animal attract a mate? (A) hibernation (B) camouflage (C) coloration (D) migration
+swipl -q -l extract.pl -g "extract('hibernation often helps an animal attract a mate.', 'hibernation')"
+swipl -q -l extract.pl -g "extract('camouflage often helps an animal attract a mate.', 'camouflage')"
+swipl -q -l extract.pl -g "extract('coloration often helps an animal attract a mate.', 'coloration')"
+swipl -q -l extract.pl -g "extract('migration often helps an animal attract a mate.', 'migration')"
+# QUESTION: Animals get energy for growth and repair from (A) soil (B) food (C) water (D) air
+swipl -q -l extract.pl -g "extract('animals get energy for growth and repair from soil.', 'soil')"
+swipl -q -l extract.pl -g "extract('animals get energy for growth and repair from food.', 'food')"
+swipl -q -l extract.pl -g "extract('animals get energy for growth and repair from water.', 'water')"
+swipl -q -l extract.pl -g "extract('animals get energy for growth and repair from air.', 'air')"
+# QUESTION: A car skids along the road and smoke appears to be coming from under the tires. The heat that produces the smoke is caused by (A) magnetism (B) sound (C) light (D) friction
+swipl -q -l extract.pl -g "extract('A car skids along the road and smoke appears to be coming from under the tires. the heat that produces the smoke is caused by magnetism.', 'magnetism')"
+swipl -q -l extract.pl -g "extract('A car skids along the road and smoke appears to be coming from under the tires. the heat that produces the smoke is caused by sound.', 'sound')"
+swipl -q -l extract.pl -g "extract('A car skids along the road and smoke appears to be coming from under the tires. the heat that produces the smoke is caused by light.', 'light')"
+swipl -q -l extract.pl -g "extract('A car skids along the road and smoke appears to be coming from under the tires. the heat that produces the smoke is caused by friction.', 'friction')"
