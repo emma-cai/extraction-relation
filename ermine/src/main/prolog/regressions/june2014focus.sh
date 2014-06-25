590a3c62303b2711aff5398596131b95d996d448
Kevin Humphreys <kevinh@Kevins-MacBook-Pro.local>
2014-06-25 15:51:30 -0700

    focus inputs

---
+++ ermine/src/main/prolog/regressions/june2014focus.sh
@@ -0,0 +1,100 @@
+# QUESTION: Which force causes rocks to roll downhill? (A) gravity(B) friction(C) erosion(D) magnetism
+echo 'gravity' > june2014question01a.focus
+echo 'friction' > june2014question01b.focus
+echo 'erosion' > june2014question01c.focus
+echo 'magnetism' > june2014question01d.focus
+# QUESTION: Which activity is an example of a good health habit? (A) watching television(B) smoking cigarettes(C) eating candy(D) exercising every day
+echo 'watching television' > june2014question02a.focus
+echo 'smoking cigarettes' > june2014question02b.focus
+echo 'eating candy' > june2014question02c.focus
+echo 'exercising every day' > june2014question02d.focus
+# QUESTION: A decomposer is an organism that (A) hunts and eats animals(B) migrates for the winter(C) breaks down dead plants and animals(D) uses water and sunlight to make food
+echo 'hunts and eats animals' > june2014question03a.focus
+echo 'migrates for the winter' > june2014question03b.focus
+echo 'breaks down dead plants and animals' > june2014question03c.focus
+echo 'uses water and sunlight to make food' > june2014question03d.focus
+# QUESTION: What is one way to change water from a liquid to a solid? (A) decrease the temperature (B) increase the temperature (C) decrease the mass (D) increase the mass
+echo 'decreasing the temperature' > june2014question04a.focus
+echo 'increasing the temperature' > june2014question04b.focus
+echo 'decreasing the mass' > june2014question04c.focus
+echo 'increasing the mass' > june2014question04d.focus
+# QUESTION: A student drops a ball. Which force causes the ball to fall to the ground? (A) electricity (B) friction (C) gravity (D) magnetism
+echo 'electricity' > june2014question05a.focus
+echo 'friction' > june2014question05b.focus
+echo 'gravity' > june2014question05c.focus
+echo 'magnetism' > june2014question05d.focus
+# QUESTION: What is the main source of energy for the water cycle? (A) electricity(B) erosion(C) gravity(D) sunlight
+echo 'electricity' > june2014question06a.focus
+echo 'erosion' > june2014question06b.focus
+echo 'gravity' > june2014question06c.focus
+echo 'sunlight' > june2014question06d.focus
+# QUESTION: Which instrument should a student use to measure exactly 10 milliliters (mL) of water? (A) graduated cylinder (B) pan balance (C) ruler (D) thermometer
+echo 'graduated cylinder' > june2014question07a.focus
+echo 'pan balance' > june2014question07b.focus
+echo 'ruler' > june2014question07c.focus
+echo 'thermometer' > june2014question07d.focus
+# QUESTION: Which activity is necessary for humans to maintain good health? (A) reading (B) resting (C) driving (D) talking
+echo 'reading' > june2014question08a.focus
+echo 'resting' > june2014question08b.focus
+echo 'driving' > june2014question08c.focus
+echo 'talking' > june2014question08d.focus
+# QUESTION: Green plants get the energy they need to make food from (A) air (B) sunlight (C) water (D) soil
+echo 'air' > june2014question09a.focus
+echo 'sunlight' > june2014question09b.focus
+echo 'water' > june2014question09c.focus
+echo 'soil' > june2014question09d.focus
+# QUESTION: When plants and animals die, which organisms help return nutrients to the food chain? (A) decomposers (B) predators (C) prey (D) producers
+echo 'decomposers' > june2014question10a.focus
+echo 'predators' > june2014question10b.focus
+echo 'prey' > june2014question10c.focus
+echo 'producers' > june2014question10d.focus
+# QUESTION: Which animal is preparing for a seasonal change in the environment? (A) a bat flying at night (B) a deer drinking water (C) an owl eating a mouse (D) a squirrel storing nuts
+echo 'a bat flying at night' > june2014question11a.focus
+echo 'a deer drinking water' > june2014question11b.focus
+echo 'an owl eating a mouse' > june2014question11c.focus
+echo 'a squirrel storing nuts' > june2014question11d.focus
+# QUESTION: What form of energy is being used when a person pushes a wooden block across the floor? (A) mechanical (B) magnetic (C) sound (D) electrical
+echo 'mechanical' > june2014question12a.focus
+echo 'magnetic' > june2014question12b.focus
+echo 'sound' > june2014question12c.focus
+echo 'electrical' > june2014question12d.focus
+# QUESTION: Since green plants make their own food, they are called (A) predators (B) prey (C) decomposers (D) producers
+echo 'predators' > june2014question13a.focus
+echo 'prey' > june2014question13b.focus
+echo 'decomposers' > june2014question13c.focus
+echo 'producers' > june2014question13d.focus
+# QUESTION: Which human activity most often has a harmful effect on the environment? (A) breathing (B) growing (C) planting (D) polluting
+echo 'breathing' > june2014question14a.focus
+echo 'growing' > june2014question14b.focus
+echo 'planting' > june2014question14c.focus
+echo 'polluting' > june2014question14d.focus
+# QUESTION: A dog opens its mouth and lets its tongue hang out. A human's body produces sweat. These are two ways that organisms may adjust to (A) cold temperatures (B) hot temperatures (C) a shortage of food (D) a shortage of drinking water
+echo 'cold temperatures' > june2014question15a.focus
+echo 'hot temperatures' > june2014question15b.focus
+echo 'a shortage of food' > june2014question15c.focus
+echo 'a shortage of drinking water' > june2014question15d.focus
+# QUESTION: A student is growing some plants for an experiment. She notices small white spots on the leaves. Which tool should she use to get a better look at the spots? (A) thermometer (B) hand lens (C) graduated cylinder (D) balance
+echo 'thermometer' > june2014question16a.focus
+echo 'hand lens' > june2014question16b.focus
+echo 'graduated cylinder' > june2014question16c.focus
+echo 'balance' > june2014question16d.focus
+# QUESTION: A fox grows thicker fur as a season changes. This adaptation helps the fox to (A) find food(B) keep warmer(C) grow stronger(D) escape from predators
+echo 'find food' > june2014question17a.focus
+echo 'keep warmer' > june2014question17b.focus
+echo 'grow stronger' > june2014question17c.focus
+echo 'escape from predators' > june2014question17d.focus
+# QUESTION: Which adaptation often helps an animal attract a mate? (A) hibernation (B) camouflage (C) coloration (D) migration
+echo 'hibernation' > june2014question18a.focus
+echo 'camouflage' > june2014question18b.focus
+echo 'coloration' > june2014question18c.focus
+echo 'migration' > june2014question18d.focus
+# QUESTION: Animals get energy for growth and repair from (A) soil (B) food (C) water (D) air
+echo 'soil' > june2014question19a.focus
+echo 'food' > june2014question19b.focus
+echo 'water' > june2014question19c.focus
+echo 'air' > june2014question19d.focus
+# QUESTION: A car skids along the road and smoke appears to be coming from under the tires. The heat that produces the smoke is caused by (A) magnetism (B) sound (C) light (D) friction
+echo 'magnetism' > june2014question20a.focus
+echo 'sound' > june2014question20b.focus
+echo 'light' > june2014question20c.focus
+echo 'friction' > june2014question20d.focus
